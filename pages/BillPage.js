7ea4462f56a668ae061bba0600d6d7cab4826070
unknown <vlad.andrei.rosu@gmail.com>
2021-09-11 21:30:48 +0300

    finished

--- pages/BillPage.js
+++ pages/BillPage.js
@@ -1,16 +1,18 @@
 import React,{useEffect, useState} from 'react'
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button, StyleSheet, Text, TextInput, View, KeyboardAvoidingView, Keyboard, Platform, ScrollView, AsyncStorage } from 'react-native'
 import BillUser from '../content/BillUser'
 import RNPickerSelect from "react-native-picker-select";
+import { Header, useHeaderHeight } from '@react-navigation/stack';
 
 export default function BillPage({route ,navigation}) {
     const {item} = route.params
+    const [bill,setBill] = useState(item)
     const [open,setOpen] = useState('')
     const [pickedValue,setPickedValue]=useState('')
     const [text,setText] = useState('')
     useEffect(()=>{
         navigation.setOptions({headerTitle:item.name})
-        
+        // console.log(item);
     },[])
     const picker = []
     item.users.forEach((x)=>{
@@ -19,9 +21,47 @@ export default function BillPage({route ,navigation}) {
             value:x.name
         })
     })
+    const addToBill = async () => {
+        if(!pickedValue)
+            return alert('Pick someone')
+        let payer = pickedValue
+        if(isNaN(text)||!text)
+            return alert('Fill in a valid number')
+        let sum = parseInt(text)
+        let billCopy = bill
+        let sumToAdd = Math.ceil(sum/(bill.users.length))
+        console.log(sumToAdd,sum,bill.users.length);
+        let payerIndex
+        for(let i = 0; i < billCopy.users.length; ++i)
+            if(billCopy.users[i].name===payer){
+                payerIndex = i
+                i = billCopy.users.length
+            }
+        for(let key in billCopy.users[payerIndex].others){
+            let crtIndex = billCopy.users.findIndex(obj=>obj.name===key)
+            if(billCopy.users[crtIndex].others[payer] > sumToAdd){
+                billCopy.users[crtIndex].others[payer] -= sumToAdd
+            }
+            else{
+                billCopy.users[payerIndex].others[key] += sumToAdd - billCopy.users[crtIndex].others[payer]
+                billCopy.users[crtIndex].others[payer] = 0
+            }
+        }
+        setBill(billCopy)
+        Keyboard.dismiss()
+        let allBills = await AsyncStorage.getItem('bills')
+        allBills = JSON.parse(allBills)
+        let billIndex = allBills.findIndex(obj=>obj.name===item.name)
+        allBills[billIndex] = bill
+        allBills = JSON.stringify(allBills)
+        await AsyncStorage.setItem('bills',allBills)
+        setPickedValue('')
+        setText('')
+
+    }
     return (
-        <View style={styles.container}>
-            {item.users.map((user,index)=>{
+        <ScrollView  contentContainerStyle={styles.container}>
+            {bill.users.map((user,index)=>{
                 return <BillUser open={open} setOpen={setOpen} user={user} key={index}></BillUser>
             })}
             <View style={styles.input}>
@@ -37,32 +77,38 @@ export default function BillPage({route ,navigation}) {
                 <TextInput value={text} onChangeText={(text)=>{
                     setText(text)
                 }} style={styles.textinput} />
-                <Button title='Add' />
+                <Button onPress={addToBill} title='Add' />
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        flex:1,
+        // flex:1,
         // justifyContent: 'center',
         alignItems:'center',
         color:'#c7c7c7',
+        paddingBottom:400,
+        // borderWidth:2,
+        // borderColor:'black',
     },
     input:{
         flexDirection:'row',
-        justifyContent: 'space-between',
+        justifyContent: 'space-around',
         alignItems:'center',
         marginTop:20,
         padding:10,
-        minWidth:'80%'
+        minWidth:'80%',
+        borderWidth:2,
+        borderColor:'black',
     },
     textinput:{
-        minWidth:'20%',
+        maxWidth:'20%',
         borderRadius:20,
         padding:10,
         minHeight:40,
         backgroundColor: 'white',
-    }
+        flexGrow:1
+    },
 })
